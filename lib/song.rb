f738746b078f574a66dd0675310d42ef9e29160f
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-01 00:21:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song 
+  attr_accessor :name, :title, :artist
+  def initialize
+    @title = title
+  end  
+  
+  def name(name)
+    @name = name
+  end
+  
+  def artist
+    @artist = artist
+  end
+
+end
